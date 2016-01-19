54f256691c242c838b97b21b21e381ac7b35d1ae
user <rakotolobo@gmail.com>
2016-01-19 08:21:01 +0100

    fichier audio

---
+++ audio/SCRIPTS/audio.js
@@ -0,0 +1,42 @@
+var app = angular.module('app', []);
+app.controller('ctrl', ["$scope", "$rootScope", function($scope, $rootScope)
+   {
+       var scope = $scope;
+       scope.effets = [{titre:'selectionner un effet'},{titre:'Ajout de lecteur'}];
+
+       scope.composants =[];
+       var id=1;
+
+       //
+
+       scope.change = function()
+       {
+           if ( $(".ajoutEffets option:selected").text()==='Ajout de lecteur')
+           {
+               scope.composants.push({'h6':'Lecteur audio','src':'IMAGES/img.png','id':'module'+id,'sup':'IMAGES/sup.png'});
+               scope.$apply();
+
+               var atr= $('#module'+id).attr("id");
+
+               var endpointOptions = { isSource:true, isTarget:true };
+
+               jsPlumb.draggable($('#'+atr))
+
+               jsPlumb.addEndpoint($('#'+atr), { anchors:["Right"] },endpointOptions).addClass("right");
+
+               jsPlumb.addEndpoint($('#'+atr), {anchors:["Left"]},endpointOptions,{cssClass:["plumb"]}).addClass("lobo");
+
+               id =id+1;
+
+               scope.effets = [{titre:'selectionner un effet'},{titre:'Ajout de lecteur'}];
+           }
+           jsPlumb.addEndpoint($('#output'), {anchors:["Left"]},{ isSource:true, isTarget:true });
+       };
+       //jsPlumb.addEndpoint($('#sortie'), {anchors:["Left"]});
+       //Fonction suppresion
+       scope.delete =function(index)
+       {
+           scope.composants.splice(index, 1);
+       };
+
+   }]);
