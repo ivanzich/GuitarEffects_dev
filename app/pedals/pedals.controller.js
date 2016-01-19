ab1da59408476b2c9bda5cf7f63728493be3b7d1
Florian Rouyer <rouyer.florian@gmail.com>
2016-01-19 11:33:24 +0100

    update

--- app/pedals/pedals.controller.js
+++ app/pedals/pedals.controller.js
@@ -8,15 +8,46 @@
         .module('app')
         .controller('Pedals.IndexController', Controller);
 
-    function Controller(UserService) {
-        var vm = this;
+    function Controller($scope, $rootScope) {
+        var scope = $scope;
+        scope.effets = [{titre:'selectionner un effet'},{titre:'Ajout de lecteur'}];
 
-        initController();
+        scope.composants =[];
+        var id=1;
 
-        function initController() {
+        //
 
+        scope.change = function()
+        {
+            if ( $(".ajoutEffets option:selected").text()==='Ajout de lecteur')
+            {
+                scope.composants.push({'h6':'Lecteur audio','src':'IMAGES/img.png','id':'module'+id,'sup':'IMAGES/sup.png'});
+                scope.$apply();
+
+                var atr= $('#module'+id).attr("id");
+
+                var endpointOptions = { isSource:true, isTarget:true };
+
+                jsPlumb.draggable($('#'+atr))
+
+                jsPlumb.addEndpoint($('#'+atr), { anchors:["Right"] },endpointOptions).addClass("right");
+
+                jsPlumb.addEndpoint($('#'+atr), {anchors:["Left"]},endpointOptions,{cssClass:["plumb"]}).addClass("lobo");
+
+                id =id+1;
+
+                scope.effets = [{titre:'selectionner un effet'},{titre:'Ajout de lecteur'}];
+            }
+            jsPlumb.addEndpoint($('#output'), {anchors:["Left"]},{ isSource:true, isTarget:true });
+        };
+        //jsPlumb.addEndpoint($('#sortie'), {anchors:["Left"]});
+        //Fonction suppresion
+        scope.delete =function(index)
+        {
+            scope.composants.splice(index, 1);
         }
     }
+
 
 })();
 
