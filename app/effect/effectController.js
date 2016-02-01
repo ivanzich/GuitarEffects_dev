4e40245cb91ceff93b6a6fd060795e39f7604285
user <rakotolobo@gmail.com>
2016-02-01 22:11:00 +0100

    les differents effets sont a peu pres les meme

--- app/effect/effectController.js
+++ app/effect/effectController.js
@@ -12,13 +12,24 @@
 
         var context = new AudioContext();
 
+        //Activer micro et delay
         var mic=null;
         var delay= null;
 
-
+        //Activer reverb
         var micRerb=  new p5.AudioIn();
         var reverb = new p5.Reverb()
 
+        //Activer Gain Flo;
+        var oscillator = null;
+        var lfo = null;
+        var gain =null;
+
+
+        //Activer chorus
+        var delayNodeChorus = null;
+        var oscillatorChorus = null;
+        var  gainChorus = null;
 
         //Fonction pour activer le micro
         vm.activerMicro=function()
@@ -55,7 +66,6 @@
                 feedback = context.createGain();
                 feedback.gain.value = valSlide(vm.delayFeedback.value);
 
-
                 delay.connect(feedback);
                 feedback.connect(delay);
 
@@ -67,18 +77,77 @@
             }
         }
 
+        //Activer reverb
         vm.activerReverb= function(){
-
             if(vm.reverb){
-
                 reverb.process(micRerb,10, 2);
                 micRerb.connect();
                 micRerb.start();
-
-
+            }else {
+                micRerb.disconnect();
             }
-            reverb.disconnect();
         }
+
+        //Activer  Gain Flo
+        vm.activerGainLfo = function(){
+
+            oscillator = context.createOscillator();
+            lfo = context.createOscillator();
+            context.createOscillator();
+
+            gain = context.createGain();
+
+            if(vm.GainLfo)
+            {
+                lfo.type="triangle";
+                oscillator.type="triangle";
+                //lfo.frequency.value = 300; // 3Hz: two oscillations par second
+                lfo.frequency.value = valSlide(vm.lfoSpeed.value)*5;
+                gain.gain.value = 0.5;
+                lfo.connect(gain.gain);
+                oscillator.connect(gain);
+                mic.connect(gain);
+                gain.connect(context.destination);
+                oscillator.start();
+                lfo.start();
+            }
+            else{
+                gain.disconnect();
+                oscillator.stop();
+                lfo.stop();
+            }
+        }
+
+        //Activer chorus
+        vm.activerChorus = function(){
+            delayNodeChorus = context.createDelay();
+            delayNodeChorus.delayTime.value = 0.6;
+
+            oscillatorChorus = context.createOscillator();
+            gainChorus = context.createGain();
+
+            gainChorus.gain.value= 1.0;
+
+            oscillatorChorus.type="sine";
+            oscillatorChorus.frequency.value=100;
+
+
+            oscillatorChorus.connect(gainChorus);
+            gainChorus.connect(delayNodeChorus.delayTime);
+
+            mic.connect(gainChorus);
+            mic.connect(delayNodeChorus);
+
+            delayNodeChorus.connect(context.destination);
+
+            oscillatorChorus.start(0);
+
+
+
+            if(vm.chorus){
+            }
+        }
+
 
         vm.delayTime = {
             value: 30,
@@ -102,7 +171,32 @@
             }
         };
 
-    function valSlide(slide){
+        vm.lfo = {
+            value: 0,
+            options: {
+                floor: 0,
+                ceil: 100
+            }
+        };
+
+        vm.lfoSpeed = {
+            value: 0,
+            options: {
+                floor: 0,
+                ceil: 100
+            }
+        };
+
+        vm.chorusSpeed = {
+            value: 0,
+            options: {
+                floor: 0,
+                ceil: 50
+            }
+        };
+
+
+        function valSlide(slide){
         return(slide/100);
     }
 
